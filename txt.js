f1c37476033a4577f5da9fd12521e9d15ba81560
Seth Anderson <sdande02@gmail.com>
2020-07-06 16:54:11 -0700

    initial commit

---
+++ txt.js
@@ -0,0 +1,26 @@
+
+// git init
+
+// npm install --save-dev nodemon
+
+// npm init
+
+// {
+//   "name": "dsa-big_o_notation",
+//   "version": "1.0.0",
+//   "description": "",
+//   "main": "index.js",
+//   "dependencies": {
+//     "nodemon": "^2.0.4"
+//   },
+//   "devDependencies": {},
+//   "scripts": {
+//     "start": "nodemon big_O_notation.js",
+//     "test": "echo \"Error: no test specified\" && exit 1"
+//   },
+//   "author": "",
+//   "license": "ISC"
+// }
+
+
+// npm start
